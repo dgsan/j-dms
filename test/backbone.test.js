94b1705fac17b4990700e3b3644a27764f1e7257
dgsan <dgsan@server.fake>
2013-07-19 12:11:17 -0700

    First remotely working version of JDB <--> Backbone model splicer.

--- test/backbone.test.js
+++ test/backbone.test.js
@@ -2,7 +2,6 @@ var BBlib = require("../lib/clients/backboneLib.js");
 
 var Schema = require("jugglingdb").Schema;
 
-
 var db = new Schema('memory');
 
 var dbModels = {};
@@ -22,16 +21,14 @@ var Models = BBlib.splice(dbModels);
 var user = new Models.User();
 
 user.set({name: 'Ted', bio: 'Ted eats chicken.'});
-user.save({name: 'Ted', bio: 'Ted eats chicken.'},
-    { success: function () {
-        dbModels.User.findOne({where: {name: 'Ted'}}, function (err, p) {
-            console.log('s');
-            console.log(err);
-            console.log(p);
-        });
-    }});
+
+// This will still save the whole thing since there's no patch option, I guess?
+user.save({}, { success: function () {
+    console.log('callback');
+}});
 
 dbModels.User.findOne({where: {name: 'Ted'}}, function (err, p) {
-            console.log(err);
-            console.log(p);
-        });
+    console.log('Found after save!');
+    console.log(err);
+    console.log(p);
+});
